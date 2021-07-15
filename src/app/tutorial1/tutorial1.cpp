f921ff4e18a32081aaf1c45b40eb0d4133ea981c
pllee4 <pinloon_0428@hotmail.com>
2021-07-15 23:08:37 +0800

    add src

---
+++ src/app/tutorial1/tutorial1.cpp
@@ -0,0 +1,15 @@
+/* 
+ * tutorial1.cpp
+ * Created on: Jul 15, 2021 22:39
+ * Description: 
+ * 
+ * Copyright (c) 2021 Pin Loon Lee (pllee4)
+ */ 
+
+#include "tutorial1.hpp"
+
+#include <iostream>
+
+int main() {
+    std::cout << "I am tutorial 1" << std::endl;
+}
---
+++ src/app/tutorial1/tutorial1.hpp
@@ -0,0 +1,13 @@
+/* 
+ * tutorial1.hpp
+ * Created on: Jul 15, 2021 22:40
+ * Description: 
+ * 
+ * Copyright (c) 2021 Pin Loon Lee (pllee4)
+ */ 
+
+#ifndef TUTORIAL1_HPP
+#define TUTORIAL1_HPP
+
+void Tutorial1();
+#endif /* TUTORIAL1_HPP */
