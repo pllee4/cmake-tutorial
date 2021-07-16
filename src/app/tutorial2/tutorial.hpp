f4971cb248d911db11744b1b1960dcaaf64d2d05
pllee4 <pinloon_0428@hotmail.com>
2021-07-16 22:36:34 +0800

    add tutorial2

---
+++ src/app/tutorial2/tutorial.hpp
@@ -0,0 +1,12 @@
+/* 
+ * tutorial.hpp
+ * Created on: Jul 16, 2021 22:34
+ * Description: 
+ * 
+ * Copyright (c) 2021 Pin Loon Lee (pllee4)
+ */ 
+ 
+#ifndef TUTORIAL_HPP
+#define TUTORIAL_HPP
+
+#endif /* TUTORIAL_HPP */
---
+++ src/app/tutorial2/tutorial2.cpp
@@ -0,0 +1,13 @@
+/* 
+ * tutorial2.cpp
+ * Created on: Jul 16, 2021 22:35
+ * Description: 
+ * 
+ * Copyright (c) 2021 Pin Loon Lee (pllee4)
+ */ 
+
+ #include <iostream>
+
+ int main() {
+    std::cout << "I am tutorial 2" << std::endl;
+}
