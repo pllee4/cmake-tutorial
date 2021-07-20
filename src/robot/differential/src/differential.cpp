98392fbc3d212f14b64539cfd62f9a6435a7c60c
pllee4 <pinloon_0428@hotmail.com>
2021-07-20 11:27:37 +0800

    src: robot: add example of differential robot for demo

---
+++ src/robot/differential/src/differential.cpp
@@ -0,0 +1,13 @@
+/* 
+ * differential.cpp
+ * Created on: Jul 20, 2021 11:25
+ * Description: 
+ * 
+ * Copyright (c) 2021 Pin Loon Lee (pllee4)
+ */ 
+
+#include <iostream>
+
+#include "robot.hpp"
+
+void DisplayRobotInfo() { std::cout << "I am differential robot" << std::endl; }
