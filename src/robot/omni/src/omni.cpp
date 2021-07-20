f46afeaa9806261b268079d2f4698a6a7658bfda
pllee4 <pinloon_0428@hotmail.com>
2021-07-20 11:27:49 +0800

    src: robot: add example of omni robot for demo

---
+++ src/robot/omni/src/omni.cpp
@@ -0,0 +1,13 @@
+/* 
+ * omni.cpp
+ * Created on: Jul 20, 2021 11:25
+ * Description: 
+ * 
+ * Copyright (c) 2021 Pin Loon Lee (pllee4)
+ */ 
+ 
+#include <iostream>
+
+#include "robot.hpp"
+
+void DisplayRobotInfo() { std::cout << "I am omni robot" << std::endl; }
