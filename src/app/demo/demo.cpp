ea3f6759ad5feb2e56c9ebd62927c1dccbd79231
pllee4 <pinloon_0428@hotmail.com>
2021-07-21 07:47:05 +0800

    src: app: add demo app

---
+++ src/app/demo/demo.cpp
@@ -0,0 +1,13 @@
+/* 
+ * demo.cpp
+ * Created on: Jul 20, 2021 11:25
+ * Description: 
+ * 
+ * Copyright (c) 2021 Pin Loon Lee (pllee4)
+ */ 
+
+ #include "robot.hpp"
+
+int main() { 
+  DisplayRobotInfo();
+}
