2aac4f205e509f6705891e978bf7a8285825130b
pllee4 <pinloon_0428@hotmail.com>
2021-07-20 11:27:08 +0800

    src: robot: add robot interface

---
+++ src/robot/interface/robot.hpp
@@ -0,0 +1,14 @@
+/* 
+ * robot.hpp
+ * Created on: Jul 20, 2021 11:25
+ * Description: 
+ * 
+ * Copyright (c) 2021 Pin Loon Lee (pllee4)
+ */ 
+ 
+#ifndef ROBOT_HPP
+#define ROBOT_HPP
+
+void DisplayRobotInfo();
+
+#endif /* ROBOT_HPP */
